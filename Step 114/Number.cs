a911e9e1c7ea03a246904a6f0c25f3a2b114e9ed
Glen Buck <glenbuck503@gmail.com>
2019-01-09 13:04:25 -0800

    Add Step 114

---
+++ Step 114/Number.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_114
+{
+    public class Number
+    {
+        public void Addition(int firstNumber)
+        {
+            int firstNumberPlus = firstNumber + 10;
+            Console.WriteLine(firstNumberPlus);
+        }
+
+        public void Addition(decimal secondNumber)
+        {
+            decimal secondNumberPlus = secondNumber + .5m;
+            Console.WriteLine(secondNumberPlus);
+        }
+
+
+        public void Addition(string thirdNumber)
+        {
+            int thirdNumberPlus = Convert.ToInt32(thirdNumber) +500;
+            Console.WriteLine(thirdNumberPlus);
+
+                
+
+
+
+        }
+           
+    }
+}
---
+++ Step 114/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number resultNumber = new Number();
+            resultNumber.Addition(600);
+            Console.ReadLine();
+
+            Number resultNumber1 = new Number();
+            resultNumber1.Addition(10.5m);
+
+            Number restultNumber3 = new Number();
+            Console.WriteLine("Please enter a number");
+            string userInput = Console.ReadLine();
+            restultNumber3.Addition(userInput);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
